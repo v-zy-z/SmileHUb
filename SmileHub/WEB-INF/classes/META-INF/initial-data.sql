0935ec7a42f1ed3aa42ec20e0d3cb8321c736471
v-zy-z <yahir.velasquez@unl.edu.ec>
2026-02-10 23:03:06 -0500

    Add files via upload

---
+++ SmileHub/WEB-INF/classes/META-INF/initial-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuarios (identificacion, tipo_identificacion, password, rol) VALUES ('1101234567', 'CEDULA', 'admin123', 'SECRETARIA');
+
+INSERT INTO usuarios (identificacion, tipo_identificacion, password, rol) VALUES ('1107654321', 'CEDULA', 'doc123', 'DOCTOR');
+
+INSERT INTO usuarios (identificacion, tipo_identificacion, password, rol) VALUES ('1109998887', 'CEDULA', 'pac123', 'PACIENTE');
+
+COMMIT;
